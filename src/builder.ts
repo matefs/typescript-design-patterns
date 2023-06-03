8e2162212410b892d4e9406a43025d18b1894545
Mateus Schverz <mate_fs@hotmail.com>
2023-06-03 16:32:01 -0300

    Create builder.ts

---
+++ src/builder.ts
@@ -0,0 +1,30 @@
+class HotDog {
+  constructor(
+    public bread: string,
+    public ketchup?: boolean,
+    public mustard?: boolean,
+    public kraut?: boolean
+  ) {}
+
+  addKetchup() {
+    this.ketchup = true;
+    return this;
+  }
+  addMustard() {
+    this.mustard = true;
+    return this;
+  }
+  addKraut() {
+    this.kraut = true;
+    return this;
+  }
+}
+
+const myLunch = new HotDog('gluten free')
+  .addKetchup()
+  .addMustard()
+  .addKraut();
+
+console.table(myLunch)
+
+
