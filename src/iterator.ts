74a9c95c44bc5230bac9c45fd33a6a129a49e181
Mateus Schverz <mate_fs@hotmail.com>
2023-06-03 18:11:24 -0300

    Create iterator.ts

---
+++ src/iterator.ts
@@ -0,0 +1,22 @@
+// modern languages provide abstractions for iterator pattern like when you are using map 
+// pull based system (sistema de empurrar)
+// range function (witch exists on python ) does not exist on javascript, so you can create one using the code below..
+ 
+ function range(start: number, end: number, step=1) {
+  return {
+    [Symbol.iterator]() {
+      return this;
+    },
+    next() {
+      if (start < end) {
+        start = start+step;
+        return { value: start, done: false };
+      }
+      return { done: true, value: end }; 
+    }
+  }
+}
+
+for (const n of range(0, 100, 5)) {
+  console.log(n);   
+}
